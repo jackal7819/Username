60713853937761586a07112564c5f5a1dc206e75
“jackal7819” <jackal3166@gmail.com>
2023-04-23 20:16:47 +0300

    Create AddUser

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUser from './components/Users/AddUser';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <AddUser />
+        </div>
+    );
+};
+
+export default App;
